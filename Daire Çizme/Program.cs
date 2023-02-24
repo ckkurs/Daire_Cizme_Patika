ff99d491d76fac5c804533162225d427186981ab
cemkndz <120122784+ckkurs@users.noreply.github.com>
2023-02-24 19:26:29 +0300

    Daire_Cizme_Patika

---
+++ Daire Çizme/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Daire_Çizme
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Dairenin yarıçapını girin: ");
+            double yaricap = double.Parse(Console.ReadLine());
+             
+            
+            for (int x = -(int)yaricap; x <= (int)yaricap; x++)
+            {
+                for (int y = -(int)yaricap; y <= (int)yaricap; y++)
+                {
+                    if (x * x + y * y <= yaricap * yaricap)
+                    {
+                        Console.Write("*");
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+                }
+                Console.WriteLine();
+            }
+           Console.ReadKey();
+        }
+    }
+}
